eec947030add3b241467246b91e37e44161e8971
flabbyrabbit <flabbyrabbit@gmail.com>
2013-02-14 13:57:08 +0000

    Unit selection via drag fixed

--- src/uk/ac/aber/luw9/mapwars/units/UnitOverlay.java
+++ src/uk/ac/aber/luw9/mapwars/units/UnitOverlay.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.MapView.Projection;
 import org.osmdroid.views.overlay.Overlay;
 
 import uk.ac.aber.luw9.mapwars.GameMap;
@@ -14,8 +13,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.Point;
 import android.location.Location;
 import android.util.Log;
@@ -28,8 +27,8 @@ public class UnitOverlay extends Overlay {
 	private ArrayList<Unit> unitsSelected = new ArrayList<Unit>();
 	private int TAP_RADIUS = 20;
 	private int UNIT_RADIUS = 25;
-	private boolean unitSelecting, unitSelectionDone;
-	private float unitSelectionStartX, unitSelectionEndX, unitSelectionStartY, unitSelectionEndY;
+	private boolean unitSelecting;
+	private Point unitSelectionStart = new Point(), unitSelectionEnd = new Point();
 	
 	public UnitOverlay(UnitController controller, GameMap map) {
 		super(map.getApplicationContext());
@@ -101,27 +100,57 @@ public class UnitOverlay extends Overlay {
 		return false;
 	}
 	
-/*	@Override
+	@Override
 	public boolean onTouchEvent(MotionEvent event, MapView mapView) {
 		Log.i("UnitOverlayTouch",  event.toString());
 		
-		float tmpX = event.getX();
-		float tmpY = event.getY();
+		int tmpX = Math.round(event.getX());
+		int tmpY = Math.round(event.getY());
 		
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			unitSelecting = true;
-			unitSelectionStartX = tmpX;
-			unitSelectionStartY = tmpY;
+			mapView.getProjection().fromMapPixels(tmpX, tmpY, unitSelectionStart);
 		} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-			unitSelectionEndX = tmpX;
-			unitSelectionEndY = tmpY;
-			unitSelectionDone = true;
+			mapView.getProjection().fromMapPixels(tmpX, tmpY, unitSelectionEnd);
+			unitSelecting = true;
+			mapView.invalidate();
+		} else if (event.getAction() == MotionEvent.ACTION_UP) {
+			unitSelecting = false;
+			
+			
+			//look for units within this range
+			ArrayList<Unit> units = unitController.getUnits(true);
+			
+		    for (Unit unit : units) {
+		    	
+		    	GeoPoint unitPoint = unit.getLocation();
+	 
+		        Location unitLocation = new Location("point A");
+		        unitLocation.setLatitude(unitPoint.getLatitudeE6() / 1E6);
+		        unitLocation.setLongitude(unitPoint.getLongitudeE6() / 1E6);
+		        
+		        double distance = tapLocation.distanceTo(unitLocation);
+		        
+		        Log.i("UnitOverlayTap", String.valueOf(distance));
+		        
+		        if (distance < TAP_RADIUS) {
+			    	//If the unit is already selected, unselect
+			    	if (unit.isSelected()) {
+				    	unit.unselect();
+				    	unitsSelected.remove(unit);
+			        } else {
+			        	unit.select();
+			        	unitsSelected.add(unit);
+			        }
+		        }
+			}
+			
+			
 			mapView.invalidate();
 		}
 		
 		return true;
 	}
-*/
+	
 	@Override
     public void draw(Canvas canvas, MapView mapView, boolean shadow) {
        // super.draw(canvas, mapView, shadow);
@@ -158,24 +187,18 @@ public class UnitOverlay extends Overlay {
 		    Unit unit = units.get(0);
 		    
 		    mapView.getProjection().toPixels(unit.getLocation(), point);
-		    Log.i("UnitOverlaySelect", point.x + " | " + unitSelectionStartX);
 	    }
 	    
-//	  if (unitSelectionDone) {
-//		Log.i("UnitOverlayDraw", String.valueOf(unitSelectionStartX));
-//		Log.i("UnitOverlayDraw", String.valueOf(unitSelectionEndX));
-//		 Paint   mPaint = new Paint();
-//	        mPaint.setDither(true);
-//	        mPaint.setColor(Color.RED);
-//	        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-//	        mPaint.setStrokeJoin(Paint.Join.ROUND);
-//	        mPaint.setStrokeCap(Paint.Cap.ROUND);
-//	        mPaint.setStrokeWidth(2);
-//	        
-//	        Path path = new Path();
-//	        
-//	        path.moveTo(unitSelectionStartX, unitSelectionStartY);
-//	        path.lineTo(unitSelectionEndX, unitSelectionEndY);
-//	}
+		if (unitSelecting) {
+			Paint   mPaint = new Paint();
+			mPaint.setDither(true);
+			mPaint.setColor(Color.RED);
+			mPaint.setStyle(Paint.Style.STROKE);
+			//Create dashed stroke
+			mPaint.setPathEffect(new DashPathEffect(new float[] {10,10}, 0));
+			mPaint.setStrokeWidth(2);
+			
+			canvas.drawRect(unitSelectionStart.x, unitSelectionStart.y, unitSelectionEnd.x, unitSelectionEnd.y, mPaint);
+		}
     }
 }
